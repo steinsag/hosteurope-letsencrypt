0729e6e2e3fdfce04cffbf2c8ae98c681c1ceac8
Lukas Schroeder <luk4sschr0d3r@users.noreply.github.com>
2020-01-16 19:23:06 +0100

    verlaengern.py // Handling von challenge=http und 'not is_root' wie in neu.py (#4)
    * Support DNS challenge, without filename
    
    * Support non-root usage

--- verlaengern.py
+++ verlaengern.py
@@ -5,14 +5,37 @@ import os
 
 from shared import domain_list, config_file
 
+# certbot tries to write to /var/log/letsencrypt by default; because of this, running as root is required.
+# certbot Error Message:
+# Either run as root, or set --config-dir, --work-dir, and --logs-dir to writeable paths.
+is_root = os.geteuid() == 0
+home_dir = os.path.expanduser('~/.config/hosteurope-letsencrypt')
+certbot_config_dir = home_dir
+certbot_work_dir = home_dir
+certbot_logs_dir = os.path.expanduser('~/.config/hosteurope-letsencrypt/logs')
+if not is_root and not os.path.exists(certbot_logs_dir):
+    os.makedirs(certbot_logs_dir)
+
+
+# Einstellungen einlesen
 with open(config_file('einstellungen.json')) as cfg_file:
     config = json.load(cfg_file)
 staging = config['staging']
 
+challenge = config.get('preferred-challenge', 'http')
+
 # certbot Kommando zusammenbauen
-cmd = 'certbot certonly -n --manual --manual-auth-hook "python3 validate.py" --agree-tos --manual-public-ip-logging-ok'
+cmd = 'certbot certonly -n --manual --agree-tos --manual-public-ip-logging-ok'
+if 'http' == challenge:
+    cmd += ' --manual-auth-hook "python3 validate.py"'
 if staging:
     cmd += ' --staging'
+
+if not is_root:
+    cmd += ' --logs-dir ' + certbot_logs_dir
+    cmd += ' --work-dir ' + certbot_work_dir
+    cmd += ' --config-dir ' + certbot_config_dir
+
 cmd += domain_list
 
 # Sicherheitsabfrage
